be034d0f40cff29b78e473b1c9bbf036ad68d42e
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-17 11:29:07 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,18 @@
+const foodItems = [
+
+    {
+        foodImageSrc: "https://top10cairo.com/wp-content/uploads/2015/12/best-burger-restaurant-places-in-cairo-696x365.jpg", 
+        foodItemName: "Burger",
+    }, 
+    {
+        foodImageSrc:"https://www.cookingclassy.com/wp-content/uploads/2019/07/steak-marinade-12-768x1152.jpg", 
+        foodItemName: "Beef", 
+    }, 
+    {
+        foodImageSrc: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/2/0/DV1510H_fried-chicken-recipe-10_s4x3.jpg.rend.hgtvcom.826.620.suffix/1568222255998.jpeg", 
+        foodItemName: "Fried Chicken",
+    }
+
+]
+
+export default foodItems; 
