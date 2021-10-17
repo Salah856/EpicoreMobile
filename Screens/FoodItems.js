3cee641b3f6e0f0ba3e2ff2e108549c0d9473e67
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-17 11:30:13 +0200

    Create FoodItems.js

---
+++ Screens/FoodItems.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { 
+    StyleSheet, View, FlatList, 
+    TouchableOpacity, ImageBackground, 
+} from 'react-native';
+
+import foodItems from '../db'; 
+
+const FoodItems = ({ navigation })=>{
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                style={styles.flatList}
+                data={foodItems}
+                renderItem={
+                    ({ item, index }) => (
+                        <TouchableOpacity
+                            key={index}
+                            onPress={(el) => { 
+                                navigation.navigate('FoodItem', {
+                                    foodItemName: item.foodItemName,
+                                    foodImageSrc: item.foodImageSrc,
+                                }) 
+                              }
+                            }
+                        > 
+                            <ImageBackground 
+                                style={styles.imageBackground}
+                                source={{uri: item.foodImageSrc}}
+                            >
+                            </ImageBackground> 
+                        </TouchableOpacity>
+                    )
+                }
+            >
+            </FlatList>
+        <StatusBar style="auto" />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    imageBackground:{
+      flex: 1,
+      height: 100,
+      width: 300,
+      backgroundColor: "black",
+      margin: 7.5,
+      justifyContent: "center",
+      alignItems: "center"
+    }, 
+    flatList:{
+      marginTop: 150,
+    }
+});
+
+export default FoodItems; 
+
