838044662a016e0920c570380d03fd2a70e825dd
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-18 08:38:52 +0200

    Update redeemCouponSubscription.js

--- GraphQL/Subscriptions/redeemCouponSubscription.js
+++ GraphQL/Subscriptions/redeemCouponSubscription.js
@@ -2,9 +2,14 @@ import {gql} from '@apollo/client';
 
 const REDEEM_COUPON_SUBSCRIPTION = gql`
 
-
-
-
+    subscription OnRedeemedCoupon($code: Int){
+        redeemedCoupon($code){
+            text
+            code
+            clientID
+            expiryDate
+        }
+    }
 `;
 
 export default REDEEM_COUPON_SUBSCRIPTION; 
