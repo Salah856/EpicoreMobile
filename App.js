a6822f86fdbb996745f6ad1cd82da20daae2dde1
Salah Elhossiny <salah_elhossiny856@yahoo.com>
2021-10-18 05:41:18 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,26 +2,39 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import splitLink from './apolloSetup'; 
+
+const client = new ApolloClient({
+  splitLink,
+  cache: new InMemoryCache()
+});
+
+
 import FoodItems from './Screens/FoodItems';
 import FoodItem from './Screens/FoodItem';
 
+
 const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-   <NavigationContainer>
-     <Stack.Navigator>
-          <Stack.Screen 
-            name="Food Items"
-            component={FoodItems}
-            options={{ title: 'Foods' }}
-          />
-          <Stack.Screen
-            name="FoodItem"
-            component={FoodItem}
-          />
-     </Stack.Navigator>
-   </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+              <Stack.Screen 
+                name="Food Items"
+                component={FoodItems}
+                options={{ title: 'Foods' }}
+              />
+              <Stack.Screen
+                name="FoodItem"
+                component={FoodItem}
+              />
+        </Stack.Navigator>
+      </NavigationContainer>
+   </ApolloProvider>
   );
 }
 
